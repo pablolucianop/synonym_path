6147537b1cef1434180b888fb3825304edb5aa8c
pablolucianop <pablolucianop@gmail.com>
2021-11-16 13:19:29 -0300

    better animation

--- src/WordButton.js
+++ src/WordButton.js
@@ -3,16 +3,6 @@ import Badge from 'react-bootstrap/Badge'
 import Card from 'react-bootstrap/Card'
 import { useSpring, animated } from 'react-spring'
 
-function Appo(props) {
-  const propsi = useSpring({ 
-    from: { opacity: 0 },
-    to: { opacity: 1},
-    config: { duration: 1000 },
-    delay: props.index*100,
-  })
-  return <animated.div   style={propsi}> <h5>{props.synCard}</h5> </animated.div>
-}
-
 
 export function WordButton (props)  {
   const submitHandler2 = (evt) =>{
@@ -24,17 +14,20 @@ export function WordButton (props)  {
     evt.preventDefault();
     props.eee(props.synCard)
   }
-
+  const propsi = useSpring({ 
+    from: { opacity: 0 },
+    to: { opacity: 1},
+    config: { duration: 1000 },
+    delay: props.index*100,
+  })
     return (
-      <div className="p-2">
+      <animated.div style={propsi} className="p-2">
         <Card type="button" className="btn btn-light flex-row" style={props} >
-          {/* <h5><div onClick={submitHandler2}> <Appo synCard={props.synCard} index={props.index} ></Appo></div><div>{(props.eee !== undefined) ? <span className="badge text-muted" onClick={submitHandler22}>╳</span> : ''}</div></h5>  */}
-  <div className="d-flex flex-row">
-  <div className="p-2" onClick={submitHandler2}><Appo synCard={props.synCard} index={props.index} ></Appo></div>
-  {(props.eee !== undefined) ?  <div className="p-2"><span className="badge text-muted" onClick={submitHandler22}>╳</span></div> : ''}
-  </div>
-
+          <div className="d-flex flex-row">
+            <div className="p-2" onClick={submitHandler2}><animated.h5 style={propsi}>{props.synCard}</animated.h5></div>
+            {(props.eee !== undefined) ?  <div className="p-2"><span className="badge text-muted" onClick={submitHandler22}>╳</span></div> : ''}
+          </div>
         </Card>
-      </div>
+      </animated.div>
     )
 }
